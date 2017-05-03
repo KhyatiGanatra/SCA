06da7bef0463fff4ef193cb7c5361b7a3b11cdc3
Khyati Ganatra <khyati.ganatra@gmail.com>
2017-05-02 18:42:38 -0700

    Add files via upload

---
+++ database_connect.php
@@ -0,0 +1,14 @@
+<?php
+//Step1
+ $db = mysqli_connect('localhost','root','','sheet1')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+     <title> DB using PHP</title>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+</body>
+</html>
